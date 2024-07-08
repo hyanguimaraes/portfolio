4c4b670b285dc7fec5c173f87c7945f8792cc9df
Hyan Guimarães <hyangt@hotmail.com>
2024-07-08 18:21:11 -0300

    tentando fazer o odômetro.

---
+++ js/contador.js
@@ -0,0 +1,10 @@
+/*CONTADOR 1*/
+
+let odometroInicial = 0;
+function somaOdometro1(){
+    let odometro1 = document.getElementById("odometro1");
+    count.innerHTML = ++odometroInicial;
+    if (odometroInicial=== 200){
+        stop;
+    }
+}
