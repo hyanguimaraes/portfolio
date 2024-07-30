ff2b65527548e15eb73fee245224553621d82023
Hyan Guimarães <hyangt@hotmail.com>
2024-07-30 18:43:06 -0300

    Menu hamburguer funcional e estilizado.

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+//JAVASCRIPT FOR THE HAMBURGUER MENU
+
+//CAPTURING THE HTML HAMBURGUER MENU BUTTON
+const buttonHamburguerMenu = document.querySelector('.header-hamburguer-menu-button');
+
+//CAPTURING THE HTML HAMBURGUER MENU OPTIONS
+const hamburguerMenuOptions = document.querySelector('.hamburguer-menu-options-container');
+
+//MONITORING CLICKS ON THE HAMBURGUER MENU BUTTON
+buttonHamburguerMenu.addEventListener('click', ()=> {
+    //IF CLICKED, ADD THE FOLLOWING CLASSES TO THE HTML COMPONENTS.
+    buttonHamburguerMenu.classList.toggle('header-hamburguer-menu-button-active');
+    hamburguerMenuOptions.classList.toggle('hamburguer-menu-options-container-active');
+});
